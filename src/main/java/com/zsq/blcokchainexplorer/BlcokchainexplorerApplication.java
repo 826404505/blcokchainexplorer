8eec278d017a2ed73393c2f9085a1248ff514dfa
826404505 <826404505@qq.com>
2019-04-17 10:53:27 +0800

    first create

---
+++ src/main/java/com/zsq/blcokchainexplorer/BlcokchainexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.zsq.blcokchainexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlcokchainexplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlcokchainexplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsq/blcokchainexplorer/BlcokchainexplorerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zsq.blcokchainexplorer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlcokchainexplorerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
